c24605f7de167a7e073238567eb12c210ee256c8
Onyekachi Godswill <omenukooyekachi690@gmail.com>
2020-12-27 12:19:16 +0100

    Updates

---
+++ precache-manifest.93ea2cf6eddc8dd8a96aad8fd6b3ee92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a73b3ad2309610881b0439857f9979",
+    "url": "/Online-books/index.html"
+  },
+  {
+    "revision": "e46dbf321a7bd2205bf9",
+    "url": "/Online-books/static/css/2.9db35e51.chunk.css"
+  },
+  {
+    "revision": "4cba4036c458cb2b17f2",
+    "url": "/Online-books/static/css/main.9b42133a.chunk.css"
+  },
+  {
+    "revision": "e46dbf321a7bd2205bf9",
+    "url": "/Online-books/static/js/2.c6ec2cfe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Online-books/static/js/2.c6ec2cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cba4036c458cb2b17f2",
+    "url": "/Online-books/static/js/main.d86c7924.chunk.js"
+  },
+  {
+    "revision": "5e47ac8a7f554de34d03",
+    "url": "/Online-books/static/js/runtime-main.ba8e0639.js"
+  }
+]);
